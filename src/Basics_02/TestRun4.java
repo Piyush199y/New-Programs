7136135317f2ca3527b0f4a901b0172f2871d6c5
Piyush199y <piyusha.bansod@gmail.com>
2024-02-15 02:22:42 +0530

    set

---
+++ src/Basics_02/TestRun4.java
@@ -0,0 +1,15 @@
+package src.Basics_02;
+
+public class TestRun4{
+    public static void print(String message) {
+        System.out.println(message);
+    }
+
+    public static void main(String[] args) {
+
+        print("Without Sout command");
+    }
+}
+
+
+
