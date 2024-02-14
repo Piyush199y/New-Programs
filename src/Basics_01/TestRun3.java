dade39b7887b0a5424fb232720acc57efe2a3dc5
Piyush199y <piyusha.bansod@gmail.com>
2024-02-15 02:05:46 +0530

    new

---
+++ src/Basics_01/TestRun3.java
@@ -0,0 +1,10 @@
+package src.Basics_01;
+
+public class TestRun3 {
+    public static void main(String[] args) {
+        int d = test(5,10) ;
+        System.out.println(d);
+    }
+    static int test (int a, int b){
+    return a + b;}
+}
