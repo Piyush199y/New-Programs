1e2cd176d1e478c0547d760c76d6e6981cc6b4e4
Piyush199y <piyusha.bansod@gmail.com>
2024-02-21 20:27:08 +0530

    oops

---
+++ src/Basics_03/Lab009.java
@@ -0,0 +1,19 @@
+package src.Basics_03;
+
+public class Lab009 {
+    public static void main(String[] args) {
+
+        Person p = new Person();
+        p.name = "Java";
+        System.out.println(p.name);
+
+        Person p2 = new Person();
+        p2.name = "JavaTwo";
+        System.out.println(p2.name);
+
+        // Person - Class blueprint
+        // p - ref variable
+        // new Person() - Real Object
+
+    }
+}
---
+++ src/Basics_03/Person.java
@@ -0,0 +1,26 @@
+package src.Basics_03;
+
+public class Person {
+    // Attribute
+
+    String name;
+    long phone_num;
+    double height;
+    int age;
+    String address;
+    String gender;
+
+    // Behaviour - What you can do?
+
+    void sleep(){
+        System.out.println("Wants to sleep");
+    }
+    void talk(){
+        System.out.println("Can talk");
+    }
+    boolean isMarried(){
+        return true;
+    }
+
+
+}
