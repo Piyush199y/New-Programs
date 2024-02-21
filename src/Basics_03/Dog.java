dd69dc1738e6fbaea2b9623ee68648de7d472b1a
Piyush199y <piyusha.bansod@gmail.com>
2024-02-21 20:54:27 +0530

    Oops

---
+++ src/Basics_03/Dog.java
@@ -0,0 +1,18 @@
+package src.Basics_03;
+
+public class Dog {
+
+    // Attributes  or data member or variable
+    String name;
+
+    // Behaviours or Methods
+    void eat(){
+        System.out.println("Dog will eat is a behaviour/method");
+    }
+
+    // Constructor
+    Dog(){
+        System.out.println("This is constructor");
+    }
+
+}
--- src/Basics_03/Lab009.java
+++ src/Basics_03/Lab009.java
@@ -3,13 +3,17 @@ package src.Basics_03;
 public class Lab009 {
     public static void main(String[] args) {
 
-        Person p = new Person();
+        Person p = new Person();  // Object
         p.name = "Java";
         System.out.println(p.name);
 
-        Person p2 = new Person();
+        Person p2 = new Person(); // Object
         p2.name = "JavaTwo";
         System.out.println(p2.name);
+
+        new Person();  // Object without reference
+        Person p3 = null; // No object will not take any memory
+
 
         // Person - Class blueprint
         // p - ref variable
