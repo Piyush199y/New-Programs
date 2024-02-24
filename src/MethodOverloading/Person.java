7d592fb35c134da8ea094ce2a339160ea75d5671
Piyush199y <piyusha.bansod@gmail.com>
2024-02-24 12:52:24 +0530

    Method overloading

---
+++ src/MethodOverloading/Person.java
@@ -0,0 +1,13 @@
+package src.MethodOverloading;
+
+public class Person {
+
+    void speak(String a){
+        System.out.println("Speak string");
+
+    }
+
+    void speak(int b){
+        System.out.println("Speak int");
+    }
+}
---
+++ src/MethodOverloading/RC.java
@@ -0,0 +1,10 @@
+package src.MethodOverloading;
+
+public class RC {
+    public static void main(String[] args) {
+
+        // Method Overloading
+        new Person().speak("String");
+        new Person().speak(20);
+    }
+}
