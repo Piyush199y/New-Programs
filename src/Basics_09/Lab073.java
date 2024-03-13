c80e9d16d8db503fdd2dad28eee7633fbe084125
Piyush199y <piyusha.bansod@gmail.com>
2024-03-13 23:14:32 +0530

    new

---
+++ src/Basics_09/Lab073.java
@@ -0,0 +1,11 @@
+package src.Basics_09;
+
+public class Lab073 {
+    public static void main(String[] args){
+        one();
+    }
+
+    public static void one(){
+        System.out.println("Generics");
+    }
+}
